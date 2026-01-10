8ec670e9ce53478de46f96ae8ae3eb35bdf4999f
Seb <seb.allen@protonmail.com>
2026-01-10 20:38:09 +0000

    script to log session

---
+++ ch02/session_logger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILENAME=$(date +%m_%d_%Y_%H:%M:%S).log
+
+if [[ ! -d ~/sessions ]]; then
+    mkdir ~/sessions
+fi
+
+# Starting a script session
+
+if [[ -z $SCRIPT ]]; then
+    export SCRIPT="/home/seb/sessions/${FILENAME}"
+    script -q -f "${SCRIPT}"
+fi
